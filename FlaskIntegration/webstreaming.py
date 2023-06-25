6ed283151d45a66c2d5c45ad93c67d2364e690cd
NickZemtzov <67985491+NickZemtzov@users.noreply.github.com>
2023-06-24 18:53:26 -0700

    Update webstreaming.py

--- FlaskIntegration/webstreaming.py
+++ FlaskIntegration/webstreaming.py
@@ -1,13 +1,13 @@
 from imutils.video import VideoStream
-from flask import Response
-from flask import Flask
-from flask import render_template
+from flask import Response, Flask, render_template, request
 import threading
 import argparse
 import datetime
 import imutils
 import time
 import cv2
+import serial
+import os
 
 # initialize the output frame and a lock used to ensure thread-safe
 # exchanges of the output frames (useful when multiple browsers/tabs
@@ -16,28 +16,53 @@ outputFrame = None
 lock = threading.Lock()
 # initialize a flask object
 app = Flask(__name__)
-# initialize the video stream and allow the camera sensor to
-# warmup
-#vs = VideoStream(usePiCamera=1).start()
+# initialize the video stream and allow the camera sensor to warmup
 vs = VideoStream(src=0).start()
 time.sleep(2.0)
 
+# Arduino Communication
+for i in range(3):
+    try:
+        os.system(f"sudo chmod a+rw /dev/ttyUSB{i}")
+        arduino=serial.Serial(port=f'/dev/ttyUSB{i}', baudrate=115200, timeout=0.1)
+    except Exception as e:
+        pass
+        #print(f"No Arduino Detected: {e}")
+
 @app.route("/")
 def index():
-	# return the rendered template
-	return render_template("index.html")
+    global state
+    text = request.args.get('button_text')
+    if text == "Down": # Keypress w
+        arduino.write(bytes("4",'utf-8'))
+    elif text == "Up": #Keypress s
+        arduino.write(bytes("5",'utf-8'))
+    elif text == "Left": #Keypress a
+        arduino.write(bytes("6",'utf-8'))
+    elif text == "Stop Left": #Unpress a
+        arduino.write(bytes("6",'utf-8'))
+    elif text == "Right": #Keypress d
+        arduino.write(bytes("7",'utf-8'))
+    elif text == "Stop Right": #Unpress d
+        arduino.write(bytes("7",'utf-8'))
+    elif text == "Rev On": #Keypress l-shift
+        arduino.write(bytes("3",'utf-8'))
+    elif text == "Rev Off": #Unpress l-shift
+        arduino.write(bytes("3",'utf-8'))
+    elif text == "Reload": #Keypress r
+        arduino.write(bytes("0",'utf-8'))
+    print(text)
+    return render_template("index_full.html")
 
-def detect_motion(frameCount):
+def detection():
 	# grab global references to the video stream, output frame, and
 	# lock variables
 	global vs, outputFrame, lock
     
     # loop over frames from the video stream
 	while True:
-		# read the next frame from the video stream, resize it,
-		# convert the frame to grayscale, and blur it
 		frame = vs.read()
-		frame = imutils.resize(frame, width=400)
+		frame = imutils.resize(frame, width=800)
 		with lock:
 			outputFrame = frame.copy()
 
@@ -72,25 +97,15 @@ def video_feed():
 
 
 if __name__ == '__main__':
-	# construct the argument parser and parse command line arguments
-	ap = argparse.ArgumentParser()
-	ap.add_argument("-i", "--ip", type=str, required=True,
-		help="ip address of the device")
-	ap.add_argument("-o", "--port", type=int, required=True,
-		help="ephemeral port number of the server (1024 to 65535)")
-	ap.add_argument("-f", "--frame-count", type=int, default=32,
-		help="# of frames used to construct the background model")
-	args = vars(ap.parse_args())
-	# start a thread that will perform motion detection
-	t = threading.Thread(target=detect_motion, args=(
-		args["frame_count"],))
+	t = threading.Thread(target=detection)
 	t.daemon = True
 	t.start()
 	# start the flask app
-	app.run(host=args["ip"], port=args["port"], debug=True,
-		threaded=True, use_reloader=False)
+	app.run(host='0.0.0.0', port=8000, debug=True,
+    		threaded=True, use_reloader=False)
 # release the video stream pointer
 vs.stop()
+
 
 
 
